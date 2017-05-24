61dd3e4fe0045cd9dd59cfc8d22def3bdc3ffbce
Dmitriy Yurov <narkozzforb@gmail.com>
2017-05-24 17:11:54 +0300

    fix launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,7 +1,7 @@
 :loop
 taskkill /f /im chrome.exe /t
 timeout /t 3
-start chrome.exe --args --allow-file-access-from-files --ignore-certificate-errors --disable-application-cache -–media-cache-size=1 -–disk-cache-size=1 -- "%1"
+start chrome.exe --args --allow-file-access-from-files --ignore-certificate-errors --disable-application-cache -–media-cache-size=1 -–disk-cache-size=1
 timeout /t 300
 taskkill /f /im chrome.exe /t
 timeout /t 3
